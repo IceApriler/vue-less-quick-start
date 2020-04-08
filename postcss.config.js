2064888f62b2e4bae33349ea14ea52a9bef5543c
qimengjie <qimengjie@163.com>
2020-04-08 09:36:07 +0800

    chone: add makeRegPath

--- postcss.config.js
+++ postcss.config.js
@@ -14,8 +14,19 @@ module.exports = {
       minPixelValue: 1,
       mediaQuery: false,
       replace: true,
-      include: [new RegExp(path.join(__dirname, './src/views/mobile'))], // 只有mobile目录下才会转换（行内样式除外）
-      exclude: /(\/|\\)(node_modules)(\/|\\)/
+      include: [makeRegPath(path.join(__dirname, './src/views/mobile'))], // 只有mobile目录下才会转换（行内样式除外）
+      exclude: [makeRegPath(path.join(__dirname, './node_modules'))]
     }
   }
+}
+
+/**
+ * 生成正则路径
+ * linux: '/src/views/report' => /\/src\/views\/report/
+ * windows: '/src/views/report' => /\\src\\views\\report/
+ * @param {String} stringPath 路径
+ * @returns {RegExp} 正则路径
+ */
+function makeRegPath(stringPath) {
+  return new RegExp(stringPath.split(path.sep).join(`\\${path.sep}`))
 }
