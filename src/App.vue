8c56f76cf3a29c7e03f2f9339f84a912dd315965
qimengjie <qimengjie@163.com>
2020-04-29 10:23:00 +0800

    feat: update ant-design-vue to 1.5.3

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
-  <a-locale-provider :locale="locale">
+  <a-config-provider :locale="locale">
     <router-view></router-view>
-  </a-locale-provider>
+  </a-config-provider>
 </template>
 <script>
-import zhCN from 'ant-design-vue/lib/locale-provider/zh_CN'
+import zhCN from 'ant-design-vue/es/locale/zh_CN'
 export default {
   name: 'App',
   data() {
