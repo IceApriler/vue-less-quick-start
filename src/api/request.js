8fdf46b88a0dd379d1a4a1e45abc81faaa302e5f
qimengjie <qimengjie@163.com>
2020-08-21 14:59:26 +0800

    feat: 优化request

--- src/api/request.js
+++ src/api/request.js
@@ -25,28 +25,34 @@ axios.defaults.withCredentials = true
 axios.defaults.timeout = 15000
 
 function doAxios(opts) {
-  return axios(opts)
-    .then(({ status, data }) => {
-      if (status === 200) {
-        if (data.code === 200) {
-          return data
-        } else if (data.code === 302 || data.code === 403) {
-          router.replace('/login')
+  return new Promise((resolve, reject) => {
+    axios(opts)
+      .then(({ status, data }) => {
+        if (status === 200) {
+          if (data.code === 200) {
+            resolve(data)
+          } else if (data.code === 302 || data.code === 403) {
+            router.push('/login')
+          } else {
+            message.error(data.message || '系统错误请联系管理员', 3)
+            reject(data)
+          }
         } else {
-          message.error(data.msg || '系统错误请联系管理员', 3)
-          return Promise.reject(data)
+          reject(data)
         }
-      } else {
-        return Promise.reject(data)
-      }
-    })
-    .catch(err => {
-      message.error(err.msg || '系统错误请联系管理员', 3)
-      if (err.response && err.response.data && err.response.data.code === 302) {
-        router.replace('/login')
-      }
-      return Promise.reject(err)
-    })
+      })
+      .catch(err => {
+        message.error(err.message || '系统错误请联系管理员', 3)
+        if (
+          err.response &&
+          err.response.data &&
+          err.response.data.code === 302
+        ) {
+          router.push('/login')
+        }
+        reject(err)
+      })
+  })
 }
 
 export function request(url, { method, data = {}, baseUrl }) {
