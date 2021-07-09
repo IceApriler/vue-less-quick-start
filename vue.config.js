323a65e3637cd8edc336a6064efe2fb592d81f3e
李京 <lijing@ylbzj.hebei.gov.cn>
2021-07-09 17:27:58 +0800

    优化打包

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,7 @@
 const path = require('path')
-
+const CompressionWebpackPlugin = require('compression-webpack-plugin')
+const productionGzipExtensions = ['js', 'css']
+const Timestamp = new Date().getTime()
 const isPro = process.env.VUE_APP_ENV === 'prod'
 
 module.exports = {
@@ -27,6 +29,20 @@ module.exports = {
         '@assets': path.join(__dirname, './src/assets'),
         '@images': path.join(__dirname, './src/assets/images'),
       },
+    },
+    plugins: [
+      // 配置compression-webpack-plugin压缩
+      new CompressionWebpackPlugin({
+        algorithm: 'gzip',
+        test: new RegExp('\\.(' + productionGzipExtensions.join('|') + ')$'),
+        threshold: 10240,
+        minRatio: 0.8,
+      }),
+    ],
+    output: {
+      // 输出重构  打包编译后的 文件名称  【模块名称.时间戳】
+      filename: `js/[name].${Timestamp}.js`,
+      chunkFilename: `js/[name].${Timestamp}.js`,
     },
   },
 }
