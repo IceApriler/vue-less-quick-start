498995f840a6c9750b7f55ac028adcc787ee0b65
qimengjie <qimengjie@163.com>
2022-06-27 17:13:57 +0800

    clean

--- src/config/index.js
+++ src/config/index.js
@@ -5,11 +5,10 @@ console.log(VUE_APP_ENV)
 let baseApiUrl
 switch (VUE_APP_ENV) {
   case 'dev':
-    baseApiUrl = 'http://111.62.218.53:80/fupin'
-    // baseApiUrl = 'http://192.168.1.58:82/fupin'
+    baseApiUrl = ''
     break
   case 'prod':
-    baseApiUrl = 'http://111.62.218.53:80/fupin'
+    baseApiUrl = ''
     break
   case 'qa':
     baseApiUrl = ''
